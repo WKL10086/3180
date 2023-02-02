7b48f021a0f69e9b3cf759cd71c75460a012f8eb
WKL10086 <w64935656@gmail.com>
2023-02-02 23:57:33 +0800

    Add personal Data

--- a1/bun.cpp
+++ a1/bun.cpp
@@ -14,9 +14,9 @@
  * and regulations, as contained in the website
  * http://www.cuhk.edu.hk/policy/academichonesty/
  *
- * Name: <FILL YOUR NAME HERE>
- * Student ID: <FILL YOUR STUDENT ID HERE>
- * Email Address: <FILL YOUR EMAIL ADDRESS HERE>
+ * Name: Wong Kai Lok
+ * Student ID: 1155125720
+ * Email Address: 1155125720@link.cuhk.edu.hk
  *
  * Source material acknowledgements (if any):
  *
